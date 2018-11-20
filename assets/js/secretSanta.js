11f30dfeb99ee21fc99232d6c21c44af8475c148
stephenrlouie <stephen.r.louie@gmail.com>
2018-11-20 08:50:33 -0500

    sending emails and submitting users, need better checks on inputs
    Reload the page

--- assets/js/secretSanta.js
+++ assets/js/secretSanta.js
@@ -2,24 +2,50 @@ function submitData() {
     console.log('Submitting Secret Santa');
     var xhr = new XMLHttpRequest();
 
-    adminName = document.getElementById("adminName").value
-    adminEmail = document.getElementById("adminEmail").value
-    spendingLimit = document.getElementById("spendingLimit").value
-    date = document.getElementById("date").value
+    // collecting high level data
+    adminName = document.getElementById("adminName").value;
+    adminEmail = document.getElementById("adminEmail").value;
+    spendingLimit = document.getElementById("spendingLimit").value;
+    date = document.getElementById("date").value;
+    title = document.getElementById("title").value;
     // check if empty ^
 
-    obj = {"adminEmail": adminEmail, "adminName": adminName, "Limit": spendingLimit, "date": date}
+    obj = {"adminEmail": adminEmail, "adminName": adminName, "limit": spendingLimit, "date": date, "title": title};
 
+    stuff = fetchMembers();
+    console.log(stuff);
+    obj.groups = stuff;
+    
 
-    // xhr.open('POST', '', true);
-    // xhr.setRequestHeader('Content-Type', 'application/json');
-    // xhr.setRequestHeader('X-Api-Key', 'cUcgmypG1F8GT4STl7y1O5zPwEdkGCTqhkut6vY8');
-    // xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://stephenrlouie.github.io');
-    // xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type,X-Api-Key,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Access-Control-Allow-Methods');
-    // xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
-    //xhr.send('');
-    console.log(JSON.stringify(obj))
-    console.log("Not sending")
+    xhr.open('POST', 'https://aw5tpxcwq1.execute-api.us-east-1.amazonaws.com/prod/SecretSanta', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Api-Key', 'cUcgmypG1F8GT4STl7y1O5zPwEdkGCTqhkut6vY8');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://stephenrlouie.github.io');
+    xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type,X-Api-Key,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Access-Control-Allow-Methods');
+    xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
+    xhr.send(JSON.stringify(obj));
+    console.log(JSON.stringify(obj));
+    console.log("Sent");
+    alert("Thank you for submitting your secret santa!")
+    location.reload()
+}
+
+function fetchMembers(){
+    groupCount = document.getElementsByClassName("groups").length;
+    groupArray = new Array(groupCount)
+    for (var g = 0; g < groupCount; g++) {
+        groupNum = g + 1;
+        memCount = document.getElementsByClassName("mem-group-" + groupNum).length;
+        groupArray[g] = new Array(memCount)
+        for (var m = 0; m < memCount; m++){
+            memNum = m + 1;
+            n = document.getElementById("name-mem-" + memNum + "-group-" + groupNum).value
+            e = document.getElementById("email-mem-" + memNum + "-group-" + groupNum).value
+            mem = {"name": n, "email": e}
+            groupArray[g][m] = mem
+        }
+    }
+    return groupArray
 }
 
 function addGroup(){
