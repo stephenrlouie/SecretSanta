d3012f47252c59768ac6ab0d4c2c82839706c1d5
Steve Louie <stephen.r.louie@gmail.com>
2018-11-20 08:37:17 -0500

    testing new tags (#2)

--- assets/js/secretSanta.js
+++ assets/js/secretSanta.js
@@ -1,15 +1,21 @@
-// On mouse-over, execute myFunction
 function submitData() {
     console.log('Submitting Secret Santa');
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', '', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('X-Api-Key', '');
-    xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://stephenrlouie.github.io');
-    xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type,X-Api-Key,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Access-Control-Allow-Methods');
-    xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
-    xhr.send('');
-    console.log("Sent?")
+
+    adminName = document.getElementById("adminName")
+    adminEmail = document.getElementById("adminEmail")
+    spendingLimit = document.getElementById("spendingLimit")
+    date = document.getElementById("date")
+    obj = {"adminEmail": adminEmail, "adminName": adminName, "Limit": spendingLimit, "date": date}
+    // xhr.open('POST', '', true);
+    // xhr.setRequestHeader('Content-Type', 'application/json');
+    // xhr.setRequestHeader('X-Api-Key', 'cUcgmypG1F8GT4STl7y1O5zPwEdkGCTqhkut6vY8');
+    // xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://stephenrlouie.github.io');
+    // xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type,X-Api-Key,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Access-Control-Allow-Methods');
+    // xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
+    //xhr.send('');
+    console.log(JSON.stringify(obj))
+    console.log("Not sending")
 }
 
 function addGroup(){
@@ -31,7 +37,7 @@ function makeGroup() {
     div.appendChild(newGroup);
 
     var addMemButton = document.createElement('input');
-    addMemButton.id = 'add-mem-group' + index;
+    addMemButton.id = 'add-mem-group-' + index;
     addMemButton.type = 'button';
     addMemButton.value = 'Add Member';
     addMemButton.className = 'addMemButton';
@@ -48,12 +54,14 @@ function addMember(thisId){
 function makeMember(id) {
     var p = document.createElement('p');
     p.className = "mem-"+id;
+    // Get length of members
     var index = document.getElementsByClassName(p.className).length + 1;
+
     document.getElementById(id).appendChild(p);
 
     var nameInput = document.createElement('input');
     nameInput.type = "text";
-    nameInput.id = "name-" + p.className;
+    nameInput.id = index + "-name-" + p.className;
 
     var nameLabel = document.createElement('label');
     nameLabel.for = name.id 
@@ -64,7 +72,7 @@ function makeMember(id) {
 
     var emailInput = document.createElement('input');
     emailInput.type = "text";
-    emailInput.id = "email-" + p.className;
+    emailInput.id = index + "-email-" + p.className;
 
     var emailLabel = document.createElement('label');
     emailLabel.for = name.id 
